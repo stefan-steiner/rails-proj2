3feac24986b2d1d34a2bcaa8ab3c946ade3d3839
John Ang <johnang@berkeley.edu>
2016-11-09 16:04:20 -0800

    updated user model

--- berkeleyfood/app/controllers/user_controller.rb
+++ berkeleyfood/app/controllers/user_controller.rb
@@ -6,4 +6,17 @@ class UserController < ApplicationController
   def index
     @users = User.all
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to @user #here it goes to show.html.erb
+    else
+      render 'new'
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:first_name).permit(:last_name).permit(:email).permit(:address)
+  end
 end
--- berkeleyfood/db/schema.rb
+++ berkeleyfood/db/schema.rb
@@ -10,11 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161109232631) do
+ActiveRecord::Schema.define(version: 20161109235401) do
 
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email"
+    t.string   "address"
   end
 
 end
