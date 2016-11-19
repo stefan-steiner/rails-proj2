f882de944420093f5dc64a6a9464c67f05bf8eb7
John Ang <johnang@berkeley.edu>
2016-11-19 00:38:05 -0800

    fixed cart styling, added more menu items to database

--- berkeleyfood/app/controllers/cart_controller.rb
+++ berkeleyfood/app/controllers/cart_controller.rb
@@ -14,12 +14,17 @@ class CartController < ApplicationController
 		else
 			@cart.items << Item.find(params[:id])
 		end
-		redirect_to '/'
 	end
 
 	def destroy
 		@cart = current_user.cart;
 		@cart.items.delete(Item.find(params[:id]))
-		redirect_to '/cart/' + current_user.id.to_s 
+		redirect_to '/cart/' + current_user.id.to_s
 	end
 end
+
+Item.create(:item => "Wings", :quantity => 1, :price => 7, :restaurat => "Crossroads")
+Item.create(:item => "Orange Chicken Plate", :quantity => 1, :price => 7, :restaurat => "Crossroads")
+Item.create(:item => "Steak Plate", :quantity => 1, :price => 7, :restaurat => "Crossroads")
+Item.create(:item => "Hamburger", :quantity => 1, :price => 5, :restaurat => "Crossroads")
+Item.create(:item => "Chocolate Pancakes", :quantity => 1, :price => 3, :restaurat => "Crossroads")
